e378c355b5a0e45c74f860931c5b28bf3763a8b8
Reca Sarfati <rebecca_sarfati@alumni.brown.edu>
2019-08-13 17:55:45 -0400

    Add VERBOSITY constant

--- src/ModelConstructors.jl
+++ src/ModelConstructors.jl
@@ -5,7 +5,8 @@ module ModelConstructors
     import Base.isempty, Base.<, Base.min, Base.max
     export
         # distributions_ext.jl
-        BetaAlt, GammaAlt, RootInverseGamma, DegenerateMvNormal, DegenerateDiagMvTDist, MatrixNormal, <=,
+        BetaAlt, GammaAlt, RootInverseGamma, DegenerateMvNormal, DegenerateDiagMvTDist,
+        MatrixNormal, <=,
 
         # settings.jl
         Setting, get_setting,
@@ -16,16 +17,21 @@ module ModelConstructors
         # abstractmodel.jl
         AbstractModel, description,
         n_states, n_states_augmented, n_shocks_exogenous, n_shocks_expectational,
-    n_equilibrium_conditions, n_observables, n_parameters, n_parameters_steady_states, n_parameters_free, n_pseudo_observables, get_dict, get_key,
+        n_equilibrium_conditions, n_observables, n_parameters, n_parameters_steady_states,
+        n_parameters_free, n_pseudo_observables, get_dict, get_key,
         spec, subspec, saveroot, dataroot,
         data_vintage, data_id, cond_vintage,
-        logpath, workpath, rawpath, tablespath, figurespath, inpath, filestring_base, workpath, rawpath, tablespath, figurespath, logpath, savepath, inpath, filestring,
+        logpath, workpath, rawpath, tablespath, figurespath, inpath, filestring_base, workpath,
+        rawpath, tablespath, figurespath, logpath, savepath, inpath, filestring,
 
         # parameters.jl
         parameter, Transform, NullablePrior, AbstractParameter,
         Parameter, ParameterVector, ScaledParameter,
         UnscaledParameter, SteadyStateParameter, transform_to_real_line, transform_to_model_space,
-        update, update!, transform_to_model_space, transform_to_real_line, Interval, ParamBoundsError
+        update, update!, transform_to_model_space, transform_to_real_line,
+        Interval, ParamBoundsError
+
+    const VERBOSITY = Dict(:none => 0, :low => 1, :high => 2)
 
     include("parameters.jl")
     include("distributions_ext.jl")
@@ -33,5 +39,4 @@ module ModelConstructors
     include("settings.jl")
     include("defaults.jl")
     include("util.jl")
-
 end
