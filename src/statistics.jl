7f46e0a085376c594ce6b2400e50d8828f9ab78c
Reca Sarfati <rebecca_sarfati@alumni.brown.edu>
2019-08-21 16:01:56 -0400

    Adding in option to compute posterior and prior from just ParameterVector type

--- src/statistics.jl
+++ src/statistics.jl
@@ -11,9 +11,8 @@ end
 
 """
 ```
-posterior(m::AbstractDSGEModel{T}, data::Matrix{T};
-          sampler::Bool = false, catch_errors::Bool = false,
-          φ_smc = 1) where {T<:AbstractFloat}
+posterior(loglikelihood::Function, parameters::ParameterVector,
+                   data::AbstractArray; ϕ_smc::Float64 = 1.)
 ```
 
 Calculates and returns the log of the posterior distribution for `m.parameters`:
@@ -25,29 +24,27 @@ log Pr(Θ|data) = log Pr(data|Θ) + log Pr(Θ) + const
 
 ### Arguments
 
-- `m`: the model object
-- `data`: matrix of data for observables
+- `loglikelihood`: Function which takes vector of parameters and data, and
+        outputs the log-loglikelihood of a parameters given data.
+- `parameters`: Vector of parameters whose likelihood is to be evaluated
+- `data`: Matrix of data for observables
 
 ### Optional Arguments
--`sampler`: Whether metropolis_hastings or smc is the caller. If `sampler=true`,
-    the log likelihood and the transition matrices for the zero-lower-bound
-    period are also returned.
--`catch_errors`: Whether to catch errors of type `GensysError` or `ParamBoundsError`
+
 - `φ_smc`: a tempering factor to change the relative weighting of the prior and
      the likelihood when calculating the posterior. It is used primarily in SMC.
 """
-function posterior(m::AbstractDSGEModel{T}, data::AbstractArray;
-                   sampler::Bool = false, ϕ_smc::Float64 = 1.,
-                   catch_errors::Bool = false) where {T<:AbstractFloat}
-    catch_errors = catch_errors | sampler
-    like = likelihood(m, data; sampler=sampler, catch_errors=catch_errors)
-    post = ϕ_smc*like + prior(m)
+function posterior(loglikelihood::Function, parameters::ParameterVector,
+                   data::AbstractArray; ϕ_smc::Float64 = 1.)
+    like = loglikelihood(parameters, data)
+    post = ϕ_smc * like + prior(m)
     return post
 end
 
 """
 ```
-posterior!(m::AbstractDSGEModel{T}, parameters::Vector{T}, data::Matrix{T};
+posterior!(loglikelihood::Function, parameters::ParameterVector,
+           para_draw::Vector{T}, data::Matrix{T};
            sampler::Bool = false, catch_errors::Bool = false,
            φ_smc = 1) where {T<:AbstractFloat}
 ```
@@ -56,8 +53,10 @@ Evaluates the log posterior density at `parameters`.
 
 ### Arguments
 
-- `m`: The model object
-- `parameters`: New values for the model parameters
+- `loglikelihood`: a function which takes vector of parameters and data, and
+                   outputs the log-loglikelihood of a parameters given data.
+
+- `para_draw`: New values for the model parameters
 - `data`: Matrix of input data for observables
 
 ### Optional Arguments
@@ -69,13 +68,14 @@ Evaluates the log posterior density at `parameters`.
 - `φ_smc`: a tempering factor to change the relative weighting of the prior and
      the likelihood when calculating the posterior. It is used primarily in SMC.
 """
-function posterior!(m::AbstractDSGEModel{T}, parameters::Vector{T}, data::AbstractArray;
-                    sampler::Bool = false, ϕ_smc::Float64 = 1.,
-                    catch_errors::Bool = false) where {T<:AbstractFloat}
+function posterior!(loglikelihood::Function, parameters::ParameterVector,
+                    para_draw::Vector{T}, data::Matrix{T};
+                    sampler::Bool = false, catch_errors::Bool = false,
+                    φ_smc::Float64 = 1.) where {T<:AbstractFloat}
     catch_errors = catch_errors | sampler
     if sampler
         try
-            DSGE.update!(m, parameters)
+            update!(parameters, para_draw)
         catch err
             if isa(err, ParamBoundsError)
                 return -Inf
@@ -84,7 +84,7 @@ function posterior!(m::AbstractDSGEModel{T}, parameters::Vector{T}, data::Abstra
             end
         end
     else
-        DSGE.update!(m, parameters)
+        DSGE.update!(parameters, para_draw)
     end
-    return posterior(m, data; sampler=sampler, ϕ_smc=ϕ_smc, catch_errors=catch_errors)
+    return posterior(loglikelihood, data; ϕ_smc = ϕ_smc)
 end
