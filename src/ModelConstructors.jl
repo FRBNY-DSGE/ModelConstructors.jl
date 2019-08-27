cd185e74cc6d4cb24def26e35302974a42c6fa21
Ethan Matlin <ethanmatlin@gmail.com>
2019-08-27 11:32:48 -0400

    Trying to resolve strange mac replacing module loop

--- src/ModelConstructors.jl
+++ src/ModelConstructors.jl
@@ -1,9 +1,10 @@
-isdefined(Base, :__precompile__) && __precompile__(false)
+isdefined(Base, :__precompile__) && __precompile__()
 
 module ModelConstructors
 
     using DataFrames, Dates, Distributed, Distributions, Nullables, Printf, Random
     using SpecialFunctions
+    using LinearAlgebra
 
     import Base.isempty, Base.<, Base.min, Base.max
     export
