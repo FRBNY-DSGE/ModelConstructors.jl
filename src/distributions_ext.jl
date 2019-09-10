8875751f5adc31addd741d7a58bed5d1a50f579c
Reca Sarfati <rebecca_sarfati@alumni.brown.edu>
2019-09-10 15:43:23 -0400

    Stop overriding MvNormal methods

--- src/distributions_ext.jl
+++ src/distributions_ext.jl
@@ -159,7 +159,7 @@ length(d::DegenerateMvNormal)
 
 Returns the dimension of `d`.
 """
-Base.length(d::Union{DegenerateMvNormal,MvNormal}) = length(d.μ)
+Base.length(d::DegenerateMvNormal) = length(d.μ)
 
 """
 ```
@@ -168,7 +168,7 @@ Distributions.rand(d::DegenerateMvNormal; cc::T = 1.0) where T<:AbstractFloat
 
 Generate a draw from `d` with variance optionally scaled by `cc^2`.
 """
-function Distributions.rand(d::Union{DegenerateMvNormal,MvNormal}; cc::T = 1.0) where T<:AbstractFloat
+function Distributions.rand(d::DegenerateMvNormal; cc::T = 1.0) where T<:AbstractFloat
     return d.μ + cc*d.σ*randn(length(d))
 end
 
@@ -180,7 +180,7 @@ Distributions.rand(d::DegenerateMvNormal, n::Int)
 Generate `n` draws from `d`. This returns a matrix of size `(length(d), n)`,
 where each column is a sample.
 """
-function Distributions.rand(d::Union{DegenerateMvNormal,MvNormal}, n::Int)
+function Distributions.rand(d::DegenerateMvNormal, n::Int)
     return d.μ .+ d.σ*randn(length(d), n)
 end
 
